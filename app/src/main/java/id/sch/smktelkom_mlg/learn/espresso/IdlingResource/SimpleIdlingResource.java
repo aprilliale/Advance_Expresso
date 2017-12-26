d7c917530f811924ded78e55fd0b9189ff654bab
eunhyukee44 <aprillia.le@gmail.com>
2017-12-26 20:21:07 +0700

    AddIdlingResourceMenuActivityTest

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/espresso/IdlingResource/SimpleIdlingResource.java
@@ -0,0 +1,40 @@
+package id.sch.smktelkom_mlg.learn.espresso.IdlingResource;
+
+import android.support.annotation.Nullable;
+import android.support.test.espresso.IdlingResource;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by rongrong on 26/12/2017.
+ */
+
+public class SimpleIdlingResource implements IdlingResource {
+
+    @Nullable
+    private volatile ResourceCallback mCallback;
+
+    private AtomicBoolean mIsIdleNow = new AtomicBoolean(true);
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        return mIsIdleNow.get();
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback callback) {
+        mCallback = callback;
+    }
+
+    public void setIdleState(boolean isIdleNow) {
+        mIsIdleNow.set(isIdleNow);
+        if (isIdleNow && mCallback != null) {
+            mCallback.onTransitionToIdle();
+        }
+    }
+}
